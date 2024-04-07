519a80e1251d334ddc88a422456adcfc7e555f25
job <arhizmei@gmail.com>
2024-04-07 12:21:18 +0300

    add ShoppingListDao

---
+++ app/src/main/java/com/kravchenkovadim/shoppinglist/data/AddItemDao.kt
@@ -0,0 +1,21 @@
+package com.kravchenkovadim.shoppinglist.data
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface AddItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: AddItem)
+    @Delete
+    suspend fun deleteItem(item: AddItem)
+    @Query("SELECT * FROM add_item WHERE listId = :listId")
+    fun getAllItemsById(listId: Int): Flow<List<AddItem>>
+    @Query("SELECT * FROM shop_list_item WHERE id = :listId")
+    suspend fun getListItemById(listId: Int): ShoppingListItem
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: ShoppingListItem)
+
+}
