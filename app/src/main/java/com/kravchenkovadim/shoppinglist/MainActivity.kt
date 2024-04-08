7a1fb216f0f66cca385ac3e4fdf8a9ce15ace75a
Arhizmei <arhizmei@gmail.com>
2024-04-08 17:21:42 +0300

    add Main Screen

--- app/src/main/java/com/kravchenkovadim/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/shoppinglist/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.kravchenkovadim.shoppinglist.main_screen.MainScreen
 import com.kravchenkovadim.shoppinglist.ui.theme.ShoppingListTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,25 +25,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShoppingListTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/kravchenkovadim/shoppinglist/main_screen/MainScreen.kt
@@ -0,0 +1,23 @@
+package com.kravchenkovadim.shoppinglist.main_screen
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    Scaffold(bottomBar = {
+
+    },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                
+            }
+        }
+
+    ) {
+
+    }
+}
