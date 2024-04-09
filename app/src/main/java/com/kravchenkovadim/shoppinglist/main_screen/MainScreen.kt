590b8818a9ed272612d658ecf26195a9bd2f0c1d
Arhizmei <arhizmei@gmail.com>
2024-04-09 23:33:02 +0300

    add button and change main color

--- app/src/main/java/com/kravchenkovadim/shoppinglist/main_screen/MainScreen.kt
+++ app/src/main/java/com/kravchenkovadim/shoppinglist/main_screen/MainScreen.kt
@@ -1,21 +1,34 @@
 package com.kravchenkovadim.shoppinglist.main_screen
 
 import android.annotation.SuppressLint
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.kravchenkovadim.shoppinglist.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
-    Scaffold(bottomBar = {
+    Scaffold(
+        bottomBar = {
 
-    },
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = { }) {
-                
+                Icon(
+                    painter = painterResource(
+                        id = R.drawable.add_icon
+                    ),
+                    contentDescription = "Add",
+                    tint = Color.White
+                )
             }
-        }
+        },
+        floatingActionButtonPosition = FabPosition.Center
 
     ) {
 
