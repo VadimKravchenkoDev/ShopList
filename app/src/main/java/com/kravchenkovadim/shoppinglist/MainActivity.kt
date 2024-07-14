d2315f01d57df6bde7531a1505dd18f078b4efef
kravchenko.vadim <kravhcneko.vadim.dev@gmail.com>
2024-07-14 15:50:26 +0300

    try number 2 to change color of status bar

--- app/src/main/java/com/kravchenkovadim/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/kravchenkovadim/shoppinglist/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.lightColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.kravchenkovadim.shoppinglist.main_screen.MainScreen
 import com.kravchenkovadim.shoppinglist.ui.theme.ShoppingListTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,6 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                     MainScreen()
                 }
             }
